3c196063540b2fbf80096246cfa0111a776e7b5a
dmgrd <117730998+dmgrd@users.noreply.github.com>
2022-12-05 14:18:58 +0300

    Add files via upload

---
+++ task 2.pas
@@ -0,0 +1,12 @@
+﻿Program task2;
+var
+s,s_old,s_new:string;
+i,l_old:byte;
+begin
+write('Исходная строка: ');
+readln(s);
+write(s, ',');
+write(s, ',');
+writeln(s);
+writeln(length(s)*3+2)
+end.
